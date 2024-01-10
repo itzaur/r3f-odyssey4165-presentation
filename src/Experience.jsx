9fdf8fa97a5b34b429bb10b6c12731290ccceb2e
itzaur <preneur85@gmail.com>
2024-01-10 14:22:16 +0300

    feat: add template scene

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,28 @@
+import { Perf } from 'r3f-perf';
 import './style.css';
+import { useControls } from 'leva';
+import { OrbitControls } from '@react-three/drei';
 
 export default function Experience() {
+  const options = useControls({
+    debug: { value: false },
+    colorBackground: {
+      value: '#fcfad9',
+    },
+  });
+
   return (
     <>
-      <h1>R3F Presentation</h1>
+      {options.debug && <Perf position='top-left' />}
+
+      <OrbitControls makeDefault />
+
+      <color attach='background' args={[options.colorBackground]} />
+
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Experience from './Experience.jsx';
+import { Canvas } from '@react-three/fiber';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Experience />
+    <Canvas>
+      <Experience />
+    </Canvas>
   </React.StrictMode>
 );
